467d127f728e78fb1859991484167d68aa2dea4a
SOUMYA JAIN <soumyajain9413@gmail.com>
2025-10-05 21:48:07 +0530

    Updated-price

--- frontend/script.js
+++ frontend/script.js
@@ -2,49 +2,49 @@ const courses = [
   {
     title: "Web Development Bootcamp",
     desc: "Master HTML, CSS, JavaScript, and React to become a full-stack web developer.",
-    price: "$49",
+    price: "₹500",
     img: "./images/Web-Dev.png"
   },
   {
     title: "MERN Stack Development",
     desc: "Learn MongoDB, Express, React, and Node.js to build full-stack modern web apps.",
-    price: "$69",
+    price: "₹1100",
     img: "./images/mern-stack.png"
   },
   {
     title: "Data Science with Python",
     desc: "Learn data visualization, machine learning, and statistics using Python.",
-    price: "$59",
+    price: "₹500",
     img: "./images/DS_Python.jpg"
   },
   {
     title: "UI/UX Design Fundamentals",
     desc: "Design beautiful interfaces and enhance user experience using Figma and Adobe XD.",
-    price: "$39",
+    price: "₹600",
     img: "./images/Ui-Ux.png"
   },
   {
     title: "Digital Marketing Mastery",
     desc: "Grow your business with SEO, Google Ads, and social media strategies.",
-    price: "$45",
+    price: "₹300",
     img: "./images/Digital marketing.png"
   },
   {
     title: "Mobile App Development",
     desc: "Build Android and iOS apps using Flutter and Firebase.",
-    price: "$55",
+    price: "₹700",
     img: "./images/Mobile-App-Development.jpg"
   },
   {
     title: "Artificial Intelligence Basics",
     desc: "Understand AI, deep learning, and neural networks from scratch.",
-    price: "$65",
+    price: "₹900",
     img: "./images/Ai.jpg"
   },
  {
     title: "DevOps & CI/CD",
     desc: "Master Docker, Kubernetes, and Jenkins to automate deployment pipelines.",
-    price: "$79",
+    price: "₹1500",
     img: "./images/CI_CD.png"
   }
 ];
